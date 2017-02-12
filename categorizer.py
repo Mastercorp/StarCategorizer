c8e1f741f15b3a175bd8a04df0463f6bc17336aa
Mastercorp <manuthestar@gmail.com>
2017-02-12 16:26:05 +0100

    GUI

--- categorizer.py
+++ categorizer.py
@@ -1,108 +1,215 @@
 import urllib2
 import json
 import time
+import Tkinter
+import threading
+import os
+import shutil
 
 
-def resolve_redirects(url):
+def resolve_redirects(url, gameamount):
     try:
+        global gameTimer
         hdr = {'User-Agent': 'super happy category bot'}
         req = urllib2.Request(url, headers=hdr)
-        time.sleep(1)
+        time.sleep(0.5)
+        rightLabel.configure(text='categorized: '+str(gameTimer) + ' / ' + gameamount)
+        gameTimer += 1
         return urllib2.urlopen(req)
     except urllib2.HTTPError as e:
         if e.code != 0:
-            print 'error 429'
+            #print 'error 429'
+            rightLabel.configure(text='HTTP error, wait 20 sec')
             time.sleep(20)
+            gameTimer -= 1
+            return resolve_redirects(url, gameamount)
     raise
 
-# delete all set tags, only exception is favorite
-names = ""
-first = False
-second = False
-with open('sharedconfig.vdf') as F:
-    for idx, line in enumerate(F):
-        searchtags = str(line.lstrip())
-        searchtags = searchtags.rstrip()
-        if searchtags == '"tags"':
-            first = True
-            continue
-        if first and searchtags == '{':
-            second = True
-            continue
-        if first and second and searchtags == '}':
-            first = False
-            second = False
-            continue
-        if searchtags == '':
-            continue
-        if searchtags == '"0"\t\t"favorite"':
-            names += '\t\t\t\t\t\t"tags"\n'
-            names += '\t\t\t\t\t\t{\n'
-            names += '\t\t\t\t\t\t\t"0"\t\t"favorite"\n'
-            names += '\t\t\t\t\t\t}\n'
-            continue
-        if first and second:
-            continue
-        names += line
 
-steamapikey = 'YOURSTEAMAPIKEYHERE'
-steam64id = 'YOURSTEAM64IDHERE'
+def quit_categorizer():
+    root.quit()
 
-#http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=YOURSTEAMKEYHERE&steamid=YOURSTEAMIDHERE&format=json
-response = urllib2.urlopen('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + steamapikey +
-                           '&steamid=' + steam64id + '&format=json')
-html = response.read()
-parsed_json = json.loads(html)
-# apps + { + linebreakes and tabs
-firstappsindex = names.rfind('"apps"') + 13
 
-beginvdf =names[:firstappsindex]
-endvdf = names[firstappsindex:]
+def stop_thread():
+    global alive
+    global startButton
+    alive = False
+    startButton.config(state='normal')
 
-#add apps not present in current list
-for idx, each in enumerate(parsed_json['response']['games']):
-    appid = str(each['appid'])
-    if (names.find('"'+appid+'"')) == -1:
-        beginvdf += '\t\t\t\t\t"'+appid+'"\n'
-        beginvdf += '\t\t\t\t\t{\n'
-        beginvdf += '\t\t\t\t\t}\n'
 
-with open('sharedconfig.vdf', 'w') as F:
-    F.write(beginvdf + endvdf)
+def backup_file():
+    if not os.path.isfile('sharedconfig.vdf.bak') and os.path.isfile('sharedconfig.vdf'):
+        shutil.copy2('sharedconfig.vdf', 'sharedconfig.vdf.bak')
 
-for idx, each in enumerate(parsed_json['response']['games']):
-    appid = str(each['appid'])
-    response = resolve_redirects('http://store.steampowered.com/api/appdetails/?appids='+appid+'&filters=genres')
-    html = response.read()
-    parsed_json2 = json.loads(html)
-    if str(parsed_json2[appid]['success']) == 'True':
-        if str(parsed_json2[appid]['data']) != '[]':
-            with open('sharedconfig.vdf', 'r') as F:
-                print appid
-                names = F.read()
-                # index after gameid and first '{'
-                firstidindex = names.rfind('"'+appid+'"') + len(appid) + 10
-                beginvdf = names[:firstidindex]
-                if names.find('{', firstidindex) - names.find('}', firstidindex) > 0:
-                    lastinvdf = names[firstidindex:]
-                    beginvdf += '\t\t\t\t\t\t"tags"\n\t\t\t\t\t\t{\n'
-                    for description in parsed_json2[appid]['data']['genres']:
-                        descript = str(description['description'])
-                        id = str(description['id'])
-                        beginvdf += '\t\t\t\t\t\t\t"' + id + '"\t\t"' + descript + '"\n'
-                    beginvdf += '\t\t\t\t\t\t}\n'
-                    names = beginvdf + lastinvdf
-                elif names.find('{', firstidindex) - names.find('}', firstidindex) < 0:
-                    beginvdf = names[:names.find('}', firstidindex)-6]
-                    lastinvdf = names[names.find('}', firstidindex):]
-                    for description in parsed_json2[appid]['data']['genres']:
-                        descript = str(description['description'])
-                        id = str(description['id'])
-                        beginvdf += '\t\t\t\t\t\t\t"' + id + '"\t\t"' + descript + '"\n'
-                        names = beginvdf + lastinvdf
-            with open('sharedconfig.vdf', 'w') as F:
-                F.write(names)
-print 'all gamefiles updated'
+
+def handle_start():
+
+    def callback():
+        global alive
+        global gameTimer
+        global startButton
+        startButton.config(state='disabled')
+        gameTimer = 1
+        alive = True
+        while alive:
+            if len(e1.get()) == 32 and len(e2.get()) == 17:
+                backup_file()
+
+            # delete all set tags, only exception is favorite
+                if os.path.isfile('sharedconfig.vdf'):
+                    names = ""
+                    first = False
+                    second = False
+                    with open('sharedconfig.vdf') as F:
+                        for idx, line in enumerate(F):
+                            searchtags = str(line.lstrip())
+                            searchtags = searchtags.rstrip()
+                            if searchtags == '"tags"':
+                                first = True
+                                continue
+                            if first and searchtags == '{':
+                                second = True
+                                continue
+                            if first and second and searchtags == '}':
+                                first = False
+                                second = False
+                                continue
+                            if searchtags == '':
+                                continue
+                            if searchtags == '"0"\t\t"favorite"':
+                                names += '\t\t\t\t\t\t"tags"\n'
+                                names += '\t\t\t\t\t\t{\n'
+                                names += '\t\t\t\t\t\t\t"0"\t\t"favorite"\n'
+                                names += '\t\t\t\t\t\t}\n'
+                                continue
+                            if first and second:
+                                continue
+                            names += line
+
+                    steamapikey = str(e1.get())
+                    steam64id = str(e2.get())
+                    try:
+                        response = urllib2.urlopen('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + steamapikey +
+                                                  '&steamid=' + steam64id + '&format=json')
+                    except urllib2.HTTPError as e:
+                        if e.code == 403:
+                            rightLabel.configure(text='wrong key or id')
+                            alive = False
+                            startButton.config(state='normal')
+                            break
+                        else:
+                            rightLabel.configure(text='http error')
+                            alive = False
+                            startButton.config(state='normal')
+                            break
+
+                    html = response.read()
+                    parsed_json = json.loads(html)
+                    # apps + { + linebreakes and tabs
+                    firstappsindex = names.rfind('"apps"') + 13
+
+                    beginvdf = names[:firstappsindex]
+                    endvdf = names[firstappsindex:]
+
+                    gameamount = str(parsed_json['response']['game_count'])
+                    leftLabel.configure(text=gameamount + " games found")
+
+
+                    # add apps not present in current list
+                    for idx, each in enumerate(parsed_json['response']['games']):
+                        appid = str(each['appid'])
+                        if (names.find('"' + appid + '"')) == -1:
+                            beginvdf += '\t\t\t\t\t"' + appid + '"\n'
+                            beginvdf += '\t\t\t\t\t{\n'
+                            beginvdf += '\t\t\t\t\t}\n'
+
+                    with open('sharedconfig.vdf', 'w') as F:
+                        F.write(beginvdf + endvdf)
+
+                    for idx, each in enumerate(parsed_json['response']['games']):
+                        if not alive:
+                            break
+                        appid = str(each['appid'])
+                        response = resolve_redirects('http://store.steampowered.com/api/appdetails/?appids=' + appid + '&filters=genres', gameamount)
+                        html = response.read()
+                        parsed_json2 = json.loads(html)
+                        if str(parsed_json2[appid]['success']) == 'True':
+                            if str(parsed_json2[appid]['data']) != '[]':
+                                with open('sharedconfig.vdf', 'r') as F:
+                                    #print appid
+                                    names = F.read()
+                                    # index after gameid and first '{'
+                                    firstidindex = names.rfind('"' + appid + '"') + len(appid) + 10
+                                    beginvdf = names[:firstidindex]
+                                    if names.find('{', firstidindex) - names.find('}', firstidindex) > 0:
+                                        lastinvdf = names[firstidindex:]
+                                        beginvdf += '\t\t\t\t\t\t"tags"\n\t\t\t\t\t\t{\n'
+                                        for description in parsed_json2[appid]['data']['genres']:
+                                            descript = str(description['description'])
+                                            id = str(description['id'])
+                                            beginvdf += '\t\t\t\t\t\t\t"' + id + '"\t\t"' + descript + '"\n'
+                                        beginvdf += '\t\t\t\t\t\t}\n'
+                                        names = beginvdf + lastinvdf
+                                    elif names.find('{', firstidindex) - names.find('}', firstidindex) < 0:
+                                        beginvdf = names[:names.find('}', firstidindex) - 6]
+                                        lastinvdf = names[names.find('}', firstidindex):]
+                                        for description in parsed_json2[appid]['data']['genres']:
+                                            descript = str(description['description'])
+                                            id = str(description['id'])
+                                            beginvdf += '\t\t\t\t\t\t\t"' + id + '"\t\t"' + descript + '"\n'
+                                            names = beginvdf + lastinvdf
+                                with open('sharedconfig.vdf', 'w') as F:
+                                    F.write(names)
+                    if alive:
+                        rightLabel.configure(text='finished')
+                        alive = False
+                    else:
+                        rightLabel.configure(text='method stopped')
+                        alive = False
+                else:
+                    rightLabel.configure(text='no sharedconfig.vdf found')
+                    alive = False
+            else:
+                rightLabel.configure(text='wrong key or id')
+                alive = False
+            if not alive:
+                startButton.config(state='normal')
+                break
+    leftLabel.configure(text='')
+    rightLabel.configure(text='')
+    t = threading.Thread(target=callback)
+    t.daemon = True
+    t.start()
+
+
+root = Tkinter.Tk()
+root.iconbitmap('D:\WORKSPACE\PycharmProjects\Categorizer\logo.ico')
+root.title('StarCategorizer')
+
+apiLabel = Tkinter.Label(root, text="Steam API Key")
+apiLabel.grid(row=0)
+
+idLabel = Tkinter.Label(root, text="steamID64")
+idLabel.grid(row=1)
+leftLabel = Tkinter.Label(root, text="")
+leftLabel.grid(row=2, column=0)
+rightLabel = Tkinter.Label(root, text="")
+rightLabel.grid(row=2, column=1)
+e1 = Tkinter.Entry(root, width=34)
+e2 = Tkinter.Entry(root, width=18)
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+Tkinter.Button(root, text='Quit', command=quit_categorizer).grid(row=3, column=0)
+startButton = Tkinter.Button(root, text='Start', command=handle_start, state='normal')
+startButton.grid(row=3, column=2)
+Tkinter.Button(root, text='Cancel', command=stop_thread).grid(row=3, column=1)
+gameTimer = 1
+alive = True
+root.mainloop()
+
+
+
+
 
 
 
